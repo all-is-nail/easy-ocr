87c1eab660eab6a2466ef5893e3753c36212695f
m0v1 <zheng9feng@vip.qq.com>
2025-03-23 14:10:57 +0800

    refactor: remove unused import statement in OcrService to clean up code

--- src/main/java/org/example/easyocr/service/OcrService.java
+++ src/main/java/org/example/easyocr/service/OcrService.java
@@ -1,6 +1,5 @@
 package org.example.easyocr.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
